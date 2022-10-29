b42cf7aee2c281d6336cf35f2f8c643d04414d72
Nausher Rao <sherrao15@gmail.com>
2022-10-29 16:10:38 -0400

    Add functionality to FirstTimeActivity

--- app/src/main/java/tech/sherrao/wlu/localify/FirstTimeActivity.java
+++ app/src/main/java/tech/sherrao/wlu/localify/FirstTimeActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class FirstTimeActivity extends AppCompatActivity {
@@ -19,13 +20,29 @@ public class FirstTimeActivity extends AppCompatActivity {
         super.setContentView(R.layout.activity_first_time);
 
         prefs = super.getSharedPreferences("tech.sherrao.wlu.localify", Context.MODE_PRIVATE);
+        checkIfFirstTime();
+        updateComponents();
+    }
+
+    private void checkIfFirstTime() {
         String originLang = prefs.getString("tech.sherrao.wlu.localify.origin", null);
         if(originLang != null)
             super.startActivity(new Intent(FirstTimeActivity.this, MapsActivity.class));
+    }
 
+    private void updateComponents() {
         Spinner spinner = super.findViewById(R.id.firstTimeDropdown);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, new String[] {"EN", "US"});
         spinner.setAdapter(adapter);
 
+        Button button = super.findViewById(R.id.firstTimeButton);
+        button.setOnClickListener((view) -> {
+            String origin = adapter.getItem(spinner.getSelectedItemPosition());
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("tech.sherrao.wlu.localify.origin", origin);
+            editor.apply();
+
+            startActivity(new Intent(FirstTimeActivity.this, MapsActivity.class));
+        });
     }
 }
